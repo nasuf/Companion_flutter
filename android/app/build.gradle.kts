7afea2c76d6e1e9a8fa242dace9ef33d226a76b1
nasuf <shanian713@gmail.com>
2026-06-12 21:45:22 +0800

    Configure Android dev and prod flavors

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,21 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val releaseKeyProperties = Properties()
+val releaseKeyPropertiesFile = file(
+    System.getenv("BANSHENG_ANDROID_KEY_PROPERTIES")
+        ?: "${System.getProperty("user.home")}/.android/bansheng-release-key.properties"
+)
+val hasReleaseSigning = releaseKeyPropertiesFile.exists()
+
+if (hasReleaseSigning) {
+    releaseKeyPropertiesFile.inputStream().use { releaseKeyProperties.load(it) }
 }
 
 android {
@@ -13,6 +26,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -20,7 +34,6 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.bansheng.companion"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
@@ -29,13 +42,41 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+    flavorDimensions += "env"
+
+    productFlavors {
+        create("dev") {
+            dimension = "env"
+            applicationId = "com.bansheng.dev"
+        }
+        create("prod") {
+            dimension = "env"
+            applicationId = "com.bansheng.prod"
         }
     }
+
+    if (hasReleaseSigning) {
+        signingConfigs {
+            create("release") {
+                storeFile = file(releaseKeyProperties.getProperty("storeFile"))
+                storePassword = releaseKeyProperties.getProperty("storePassword")
+                keyAlias = releaseKeyProperties.getProperty("keyAlias")
+                keyPassword = releaseKeyProperties.getProperty("keyPassword")
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName(
+                if (hasReleaseSigning) "release" else "debug"
+            )
+        }
+    }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
